a21970ab2475c38fbb6c852d677eab7cf18c1d1b
Jeferson <jefeleal363@gmail.com>
2024-10-21 14:12:46 -0300

    feat: lógica

---
+++ Lógica/index.js
@@ -0,0 +1,21 @@
+//Variáveis
+
+console.log("Variáveis")// para testar ou imprimir se usa o console.log
+
+let pessoa = "Joao" //declaração de variavel = string
+let numero = 10 //declaração de variavel = numero
+let booleano = true //declaração de variavel = booleano
+
+console.log(pessoa, numero, booleano)
+
+pessoa = "Maria" //alterando o valor da variavel "pessoa"
+
+console.log(pessoa, numero, booleano)
+
+
+const constante = "IMUTAVEL" //variavel constante
+
+console.log(constante)
+constante = "ALTERADO" //alterando o valor da variavel "constante"
+
+console.log(constante)
