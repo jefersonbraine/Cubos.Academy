e89b4ce5e74cf351812c4f35d79e696963ce432b
Jeferson <jefeleal363@gmail.com>
2024-10-21 15:43:47 -0300

    feat: Manipulando Strings

--- Minicurso de Programação na prática/Lógica de Programação/Rev. Manipulando Strings/index.js
+++ Minicurso de Programação na prática/Lógica de Programação/Rev. Manipulando Strings/index.js
@@ -1,1 +1,23 @@
-let nome = "testando"
+console.log('Iniciando.dev'.toUpperCase())
+
+console.log("Iniciando.dev".toLowerCase());
+
+console.log("Iniciando.dev".length);
+
+console.log("Iniciando.dev".indexOf("i"));
+
+console.log("Iniciando.dev".lastIndexOf("i"));
+
+console.log("Iniciando.dev".startsWith("Iniciando"));
+
+console.log("Iniciando.dev".endsWith("dev"));
+
+console.log("Iniciando.dev".includes("i"));
+
+console.log("Iniciando.dev".replace("i", "o"));
+
+console.log("Iniciando.dev".split("."));
+
+console.log("Iniciando.dev".repeat(3));
+
+
