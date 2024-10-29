6ff916036a501810f39837c2f150f1abdc024a94
Jeferson <jefeleal363@gmail.com>
2024-10-29 14:42:34 -0300

    feat: open ai install and config

---
+++ Minicurso de Programação na prática/React-teach.me/src/api/openai.ts
@@ -0,0 +1,24 @@
+import OpenAI from "openai";
+
+type Message = {
+    role: 'user' | 'assistant'
+    content: string
+}
+
+const openai = new OpenAI({
+    apiKey: '',
+    dangerouslyAllowBrowser: true
+})
+
+export async function sendMessage(messages: Message[]) {
+    const response = await openai.chat.completions.create({
+        model: "gpt-4o-mini",
+        messages
+    })
+
+    return {
+        role: response.choices[0].message.role,
+        content: response.choices[0].message.content
+    }
+}
+
